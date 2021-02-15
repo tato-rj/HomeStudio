9d79ab9ec7d71270dae34438f73e7ebdf3a7b7cf
Arthur Villar <=>
2021-02-15 11:15:28 -0500

    Update main page

--- resources/views/sections/bio.blade.php
+++ resources/views/sections/bio.blade.php
@@ -4,4 +4,6 @@
 	<p>In 2007 he won the first prize in the Souza Lima National Piano Competition in SP, Brazil, and performed in 2009 Bach’s 24 preludes and fugues at the Forum Internacional del Piano de Barcelona. In 2013 Arthur Villar released a recording of J.S. Bach's The Well-Tempered Clavier book 1, find it <a href="https://www.amazon.com/Well-Tempered-Clavier-Book-1/dp/B00BNQ2684/" target="_blank" class="link-red">here</a>.
 
 	<p>Arthur Villar participated in the following festivals and workshops: SIPO (Portugal), Forum Internacional del Piano in Barcelona (Spain), Performing Bach in Baroque Style in New York (US), Prague Music Performance Institute and Festival in Prague (Czech Republic) and Gijon International Piano Festival (Spain), and has studied with renowned pianists, such as Andrei Gavrilov (Russia), Paul Badura-Skoda (Austria), Julian Martin (US), Boris Berman (Russia), among others.</p>
+
+	<p>Arthur Villar is also a Certified Suzuki Teacher and a member of the following organizations: National Music Teachers Association (MTNA), Suzuki Association of the Americas (SAA), Hartford Music Teachers Alliance (HMTA), and Royal Concervatory of Music (RCM).</p>
 @endcomponent
